b1458da2e875255921e1c8c40084ab8c033e1749
MZagTest <118060197+MZagTest@users.noreply.github.com>
2022-12-06 19:20:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ const fetchPicture = name => {
   const parsedName = name.trim();
   if (parsedName.length === 0) return;
   const url = getUrl(parsedName);
-  return axios(url)
+  return axios.get(url)
     .then(response => {
       // console.log(parsedName);
 
