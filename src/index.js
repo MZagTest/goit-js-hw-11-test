3f0213e797f54e1bad818b8b0128ad3cccb7f0be
MZagTest <118060197+MZagTest@users.noreply.github.com>
2022-12-07 13:27:41 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,108 +1,117 @@
-import Notiflix from 'notiflix';
-import axios from 'axios';
+import _throttle from 'lodash.throttle';
+import axios from "axios";
+import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-const axios = require('axios');
-const form = document.querySelector('#search-form');
-const input = document.querySelector('input');
-const btn = document.querySelector('button');
-const div = document.querySelector('.gallery');
-const loader = document.querySelector('#load-more');
-loader.style.visibility = 'hidden';
-const safeSearch = true;
-const theKey = '31673863-7b4e2329a784886b2ded53b03&';
-let page = 1;
-let amount = 40;
-// console.log(input);
-const getUrl = search =>
-  `https://pixabay.com/api/?key=${theKey}&q=${search}&type=photo&orientation=horizontal&safesearch=${safeSearch}&per_page=${amount}&page=${page}`;
 
-const createImageLoader = () => {
-  return (
-    (page += 1),
-    fetchPicture(input.value),
-    // console.log(`Loaded images from page ${page - 1}`),
-    (loader.style.visibility = 'visible')
-  );
+const APIKEY = "31019872-5203125bb9147bf7b31b034ba"
+const gallery = document.querySelector(".gallery");
+const form = document.querySelector("#search-form");
+const btnMore = document.querySelector(".load-more");
+const btnLoop = document.querySelector(".loop-btn");
 
-  
+let actualPage = 1;
+let currentSearchName = "";
+let lastPage = 1;
+let autoScrollToogle = false;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    scrollZoomFactor: false,});
+		
+const fetchPhotos = async (name,page) => {
+    try {
+        const respone = await axios(`https://pixabay.com/api/?key=${APIKEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+        const photos = await respone.data;
+        checkResults(photos);
+    } catch(error) {
+        console.log(error.message);
+        Notiflix.Notify.failure(error.message);
+    }
 };
 
-// const loadImages = createImageLoader();
+const checkResults = (photos) => {
+     if (photos.hits.length === 0) { 
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");   
+        } else {    
+            if (actualPage === 1) {
+                Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+                currentSearchName = name;
+                lastPage = Math.ceil(photos.totalHits / 40);
+                if (lastPage > 1 && autoScrollToogle===false){ btnMore.classList.add("is-visible")};
+            }
+            renderPhotos(photos);
+            lightbox.refresh();
+            if (actualPage > 1) {scrollAfterRender()};
+        }   
+}
 
-const fetchPicture = async name => {
-  const parsedName = name.trim();
-  if (parsedName.length === 0) return;
-  const url = getUrl(parsedName);
-  const table = await axios(url);
-  const photos = await table.data;
-  console.log('table', table);
-  testSomething(photos);
-};
+const renderPhotos =(photos) => {
+    const markup = photos.hits.map((photo) => {
+        return `<div class="photo-card">
+                    <a href="${photo.largeImageURL}"> 
+                        <img src="${photo.webformatURL}" alt="${photo.tags}" data-source="${photo.largeImageURL}" loading="lazy" />
+                        <div class="info">
+                        <p class="info-item"><span><b>Likes</b></span><span>${photo.likes}</span></p>
+                        <p class="info-item"><span><b>Views</b></span><span>${photo.views}</span></p>
+                        <p class="info-item"><span><b>Comments</b></span><span>${photo.comments}</span></p>
+                        <p class="info-item"><span><b>Downloads</b></span><span>${photo.downloads}</span></p>
+                        </div>
+                    </a> 
+                </div>`
+    })
+    .join("")
+    gallery.innerHTML = gallery.innerHTML+markup;
+}
 
-const testSomething = response => {
-  if (response.hits.length === 0) {
-    throw Notiflix.Notify.info(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-  const totalHits = response.totalHits;
-  return (
-    renderImages(response.hits),
-    // console.log(totalHits),
-    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`)
-  );
-};
+const scrollAfterRender = () => {
+    const photoInfo = document.querySelector(".gallery").firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } = photoInfo;
+    window.scrollBy({
+        top: cardHeight * 2,
+        behavior: "smooth",
+    })
+}
+ 
+const infinityScroll = _throttle(() => {
+    if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        if (actualPage < lastPage) {
+            actualPage++;
+            fetchPhotos(currentSearchName, actualPage);     
+            if (actualPage === lastPage) {
+                Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); 
+                btnMore.classList.remove("is-visible");
+            }  
+        } 
+    }
+},300)
 
-const lightbox = new SimpleLightbox(`.gallery a`, {
-  disableRightClick: true,
-  captionsData: "alt",
-  captionDelay: 250,
-});
-const renderImages = res => {
-  const img = res
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) =>
-        `<div class="photo-card">
-        <a cclass="gallery__item" href=${largeImageURL}>
-<img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-<div class="info">
-  <p class="info-item">
-    <b>Likes</b> ${likes}
-  </p>
-  <p class="info-item">
-    <b>Views</b> ${views}
-  </p>
-  <p class="info-item">
-    <b>Comments</b> ${comments}
-  </p>
-  <p class="info-item">
-    <b>Downloads</b> ${downloads}
-  </p>
-</div>
-</div>`
-    )
-    .join('');
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    actualPage = 1;
+    gallery.innerHTML = "";
+    fetchPhotos(form.searchQuery.value, actualPage);
+})
 
-  div.insertAdjacentHTML('beforeend', img);
-  loader.textContent = 'Loading...';
-};
+btnLoop.addEventListener("click", () => {
+    if (autoScrollToogle === false) {
+        autoScrollToogle = true;
+        window.addEventListener('scroll', infinityScroll);
+        Notiflix.Notify.info("Loading method has been changed to auto.");
+    } else {
+        autoScrollToogle = false;
+        window.removeEventListener('scroll', infinityScroll);
+        Notiflix.Notify.info("Loading method has been changed to manual.");    
+    }  
+    btnLoop.classList.toggle("is-active");
+    if (actualPage!=lastPage){btnMore.classList.toggle("is-visible");} 
+})
 
-const observer = new IntersectionObserver(([entry]) => {
-  if (!entry.isIntersecting) return;
-  createImageLoader();
-});
-// btn.addEventListener('click', fetchPicture(input.value));
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetchPicture(input.value);
-});
-observer.observe(loader);
+btnMore.addEventListener("click", () => {
+    actualPage++;
+    fetchPhotos(currentSearchName, actualPage);     
+    if (actualPage === lastPage) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results."); 
+        btnMore.classList.remove("is-visible");
+    }  
+})
